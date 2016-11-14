a76ab6b9a5c5c2165a935fdf96a9d0851113b903
eddy1001 <eddy1001@naver.com>
2016-11-15 00:18:29 +0900

    - 수정사항 1. 휴식, 공부, 식사 선택창 추가 2. 한 할동의 제목을 입력하는 공간 추가 3. 분석을 해주는 기능과 버튼 추가

--- app/src/main/java/com/example/mylocationloggertest01/MainActivity.java
+++ app/src/main/java/com/example/mylocationloggertest01/MainActivity.java
@@ -11,6 +11,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -50,6 +52,23 @@ public class MainActivity extends Activity implements OnMapReadyCallback {
     private PolylineOptions polylineOptions;
     private List<LatLng> arrayPoints = new ArrayList<LatLng>();
 
+    //checking
+    private RadioButton radio_Rest;
+    private RadioButton radio_Stdy;
+    private RadioButton radio_Eatt;
+
+    private int checking = 0;
+
+    private EditText edit_Name;
+
+    private int R_Num;
+    private int S_Num;
+    private int E_Num;
+
+    private TextView txt_Rrate;
+    private TextView txt_Srate;
+    private TextView txt_Erate;
+
     public void onMapReady(final GoogleMap map) {
         googleMap = map;
 
@@ -63,11 +82,19 @@ public class MainActivity extends Activity implements OnMapReadyCallback {
         //Marker seoul = googleMap.addMarker(new MarkerOptions().position(SEOUL).title("Seoul"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom( SEOUL, 15));
         googleMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
+        SaveTestting();
     }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        radio_Rest = (RadioButton) findViewById(R.id.radioButton_Rest);
+        radio_Stdy = (RadioButton) findViewById(R.id.radioButton_Study);
+        radio_Eatt = (RadioButton) findViewById(R.id.radioButton_Eat);
+
+        edit_Name = (EditText) findViewById(R.id.editText_Name);
 
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
@@ -76,6 +103,15 @@ public class MainActivity extends Activity implements OnMapReadyCallback {
         SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
         GpsCount = pref.getInt("GpsCount", 144);
         saveNum = pref.getInt("saveNum", 0);
+
+        R_Num = pref.getInt("RNum", 0);
+        S_Num = pref.getInt("SNum", 0);
+        E_Num = pref.getInt("ENum", 0);
+
+        txt_Rrate = (TextView) findViewById(R.id.textView_Rrate);
+        txt_Srate = (TextView) findViewById(R.id.textView_Srate);
+        txt_Erate = (TextView) findViewById(R.id.textView_Erate);
+
         Toast.makeText(
                 getApplicationContext(),
                 "카운트 초기화",
@@ -84,38 +120,9 @@ public class MainActivity extends Activity implements OnMapReadyCallback {
         btnShowLocation = (Button) findViewById(R.id.But_GPS);
         txtLat = (TextView) findViewById(R.id.Latitude);
         txtLon = (TextView) findViewById(R.id.Longitude);
-
-        handler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                if (GpsCount > 0){
-                    GpsCount--;
-                    SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
-                    SharedPreferences.Editor editor = pref.edit();
-                    editor.putInt("GpsCount", GpsCount);
-                    editor.commit();
-
-                    ReadGPS();
-                    this.sendEmptyMessageDelayed(0, 1000 * 60 * 10);
-                }
-                else if (GpsOn) {
-                    GpsOn = false; GpsCount = 144; saveNum = 0;
-                }
-            }
-        };
-
-        // GPS 정보를 보여주기 위한 이벤트 클래스 등록
-        btnShowLocation.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0) {
-                if (!GpsOn) {
-                    GpsOn = true;
-                    handler.sendEmptyMessage(1);
-                }
-            }
-        });
     }
 
-    public void ReadGPS(){
+    public void ReadGPS(View v){
         gps = new GpsInfo(MainActivity.this);
         // GPS 사용유무 가져오기
         if (gps.isGetLocation()) {
@@ -123,10 +130,7 @@ public class MainActivity extends Activity implements OnMapReadyCallback {
             double longitude = gps.getLongitude();
             txtLat.setText(String.valueOf(latitude));
             txtLon.setText(String.valueOf(longitude));
-            Toast.makeText(
-                    getApplicationContext(),
-                    "남은 횟수 : " + GpsCount + ", " + saveNum,
-                    Toast.LENGTH_LONG).show();
+
             // 밑으로 저장하는 코드
             SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
             SharedPreferences.Editor editor = pref.edit();
@@ -134,29 +138,64 @@ public class MainActivity extends Activity implements OnMapReadyCallback {
             editor.commit();
             editor.putFloat("Lon" + saveNum, (float) longitude);
             editor.commit();
+            editor.putString("Nam" + saveNum, edit_Name.getText().toString());
+            editor.commit();
             saveNum++;
             editor.putInt("saveNum", saveNum);
             editor.commit();
+
+            if (checking == 0){
+                R_Num++;
+                editor.putInt("RNum", R_Num);
+                editor.commit();
+                Toast.makeText(
+                        getApplicationContext(),
+                        "휴식 저장되었습니다.",
+                        Toast.LENGTH_LONG).show();
+            }
+            else if (checking == 1){
+                S_Num++;
+                editor.putInt("SNum", S_Num);
+                editor.commit();
+                Toast.makeText(
+                        getApplicationContext(),
+                        "공부 저장되었습니다.",
+                        Toast.LENGTH_LONG).show();
+            }
+            else if (checking == 2){
+                E_Num++;
+                editor.putInt("ENum", E_Num);
+                editor.commit();
+                Toast.makeText(
+                        getApplicationContext(),
+                        "식사 저장되었습니다.",
+                        Toast.LENGTH_LONG).show();
+            }
+
+            SaveTestting();
         } else {
             // GPS 를 사용할수 없으므로
             gps.showSettingsAlert();
         }
     }
 
-    public void SaveTestting(View v){
+    public void SaveTestting(){
         googleMap.clear();
         arrayPoints.clear();
 
+        EditText edt_Name = (EditText) findViewById(R.id.editText_Name);
+
         if (GoogleMapOn){
             SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
-            double latitude; double longitude;
-            for (int i = 0; i < 144; i++) {
+            double latitude; double longitude; String name;
+            for (int i = 0; i < saveNum; i++) {
                 latitude = pref.getFloat("Lat" + i, -1);
                 longitude = pref.getFloat("Lon" + i, -1);
+                name = pref.getString("Nam" + i, "NoName");
                 if (latitude > 0 && longitude > 0) {
                     SEOUL = new LatLng(latitude, longitude);
                     arrayPoints.add(SEOUL);
-                    Marker seoul = googleMap.addMarker(new MarkerOptions().position(SEOUL).title("You " + i));
+                    Marker seoul = googleMap.addMarker(new MarkerOptions().position(SEOUL).title(name));
                     if (i == 0) {
                         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 15));
                         googleMap.animateCamera(CameraUpdateFactory.zoomTo(20), 2000, null);
@@ -178,4 +217,44 @@ public class MainActivity extends Activity implements OnMapReadyCallback {
                     Toast.LENGTH_LONG).show();
         }
     }
+
+    public void radioCheckR(View v){
+        radio_Rest.setChecked(true);
+        radio_Eatt.setChecked(false);
+        radio_Stdy.setChecked(false);
+
+        checking = 0;
+    }
+    public void radioCheckS(View v){
+        radio_Rest.setChecked(false);
+        radio_Eatt.setChecked(false);
+        radio_Stdy.setChecked(true);
+
+        checking = 1;
+    }
+    public void radioCheckE(View v){
+        radio_Rest.setChecked(false);
+        radio_Stdy.setChecked(false);
+        radio_Eatt.setChecked(true);
+
+        checking = 2;
+    }
+
+    public void showRate(View v){
+        int totalrate = R_Num + S_Num + E_Num;
+
+        if (totalrate <= 0){
+            return;
+        }
+
+        float R_rating = (float) R_Num / (float)totalrate * 100;
+        txt_Rrate.setText("휴식 : " + String.format("%.2f", R_rating) + "%");
+
+        float S_rating = (float)S_Num / (float)totalrate * 100;
+        txt_Srate.setText("공부 : " + String.format("%.2f", S_rating) + "%");
+
+        float E_rating = (float)E_Num / (float)totalrate * 100;
+        txt_Erate.setText("식사 : " + String.format("%.2f", E_rating) + "%");
+    }
+
 }
